af3d882f29302cb79c2c65b1e0157c3484275cb7
lily <2510844725@qq.com>
2026-06-02 13:50:42 +0800

    Revert "fix: 同时移除 whileInView 中的 opacity:1，阻止 framer-motion 自动推断 opacity:0"
    This reverts commit 1918be4d08fe7b63acb7501c65f4f5e0ba71519e.

--- src/pages/SiteHome.tsx
+++ src/pages/SiteHome.tsx
@@ -155,7 +155,7 @@ export default function SiteHome() {
           <div className="about-content-wrapper">
             <motion.div
               initial={{ x: -50 }}
-              whileInView={{ x: 0 }}
+              whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
               className="about-text-card relative"
             >
@@ -174,7 +174,7 @@ export default function SiteHome() {
             <div className="about-image-collage relative h-[700px] translate-y-8 md:translate-y-12">
               <motion.div
                 initial={{ rotate: -10, x: -24, y: 42 }}
-                whileInView={{ rotate: -7, x: 0, y: 0 }}
+                whileInView={{ rotate: -7, x: 0, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--left w-64 h-80 top-5 left-5 z-20"
               >
@@ -184,7 +184,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: 14, x: 20, y: -36 }}
-                whileInView={{ rotate: 11, x: 0, y: 0 }}
+                whileInView={{ rotate: 11, x: 0, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--right w-62 h-42 top-12 right-10 z-10"
               >
@@ -193,7 +193,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: 5, scale: 0.84, y: 18 }}
-                whileInView={{ rotate: 2, scale: 1, y: 0 }}
+                whileInView={{ rotate: 2, scale: 1, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img w-60 h-76 top-38 right-16 z-30"
               >
@@ -202,7 +202,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: -4, x: -36, y: 20 }}
-                whileInView={{ rotate: -8, x: 0, y: 0 }}
+                whileInView={{ rotate: -8, x: 0, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--left w-54 h-56 bottom-18 left-17 z-10"
               >
@@ -211,7 +211,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: 7, scale: 0.92, x: 18 }}
-                whileInView={{ rotate: 9, scale: 1, x: 0 }}
+                whileInView={{ rotate: 9, scale: 1, x: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--right w-46 h-62 bottom-2 right-32 z-20"
               >
@@ -220,7 +220,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: -12, y: 18 }}
-                whileInView={{ rotate: -14, y: 0 }}
+                whileInView={{ rotate: -14, opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--ghost w-40 h-28 top-[78%] left-[3%] z-0 opacity-28"
               >
@@ -237,7 +237,7 @@ export default function SiteHome() {
             <div className="team-image-collage order-2 lg:order-1 relative h-[660px]">
               <motion.div
                 initial={{ rotate: 11, x: -18, y: 44 }}
-                whileInView={{ rotate: 7, x: 0, y: 0 }}
+                whileInView={{ rotate: 7, x: 0, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--left w-78 h-54 top-4 left-3 z-20"
               >
@@ -246,7 +246,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: -11, x: 20, y: -34 }}
-                whileInView={{ rotate: -8, x: 0, y: 0 }}
+                whileInView={{ rotate: -8, x: 0, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--right w-78 h-54 bottom-4 right-0 z-10"
               >
@@ -255,7 +255,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: 4, scale: 0.88, y: 18 }}
-                whileInView={{ rotate: 2, scale: 1, y: 0 }}
+                whileInView={{ rotate: 2, scale: 1, y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 className="collage-img w-70 h-50 top-28 right-16 z-30"
               >
@@ -264,7 +264,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: -6, x: 26, y: 12 }}
-                whileInView={{ rotate: -9, x: 0, y: 0 }}
+                whileInView={{ rotate: -9, opacity: 1, x: 0, y: 0 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--left w-40 h-52 bottom-14 left-16 z-20"
               >
@@ -273,7 +273,7 @@ export default function SiteHome() {
 
               <motion.div
                 initial={{ rotate: 14, scale: 0.84 }}
-                whileInView={{ rotate: 12, scale: 1 }}
+                whileInView={{ rotate: 12, opacity: 1, scale: 1 }}
                 viewport={{ once: true }}
                 className="collage-img collage-img--ghost collage-img--right w-42 h-54 top-[50%] left-[32%] z-0 opacity-24"
               >
@@ -283,7 +283,7 @@ export default function SiteHome() {
 
             <motion.div
               initial={{ x: 50 }}
-              whileInView={{ x: 0 }}
+              whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
               className="team-text-card order-1 lg:order-2 relative"
             >
@@ -310,7 +310,7 @@ export default function SiteHome() {
                       <motion.span
                         key={name}
                         initial={{ opacity: 0 }}
-                        whileInView={{}}
+                        whileInView={{ opacity: 1 }}
                         viewport={{ once: true }}
                         transition={{ delay: 0.2 + index * 0.1, duration: 0.8 }}
                         className="text-lg md:text-xl font-serif text-ink/70"
